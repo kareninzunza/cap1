29dd10da399b57f84aa1b7ac1282539de4e77353
kareninzunza <113056696+kareninzunza@users.noreply.github.com>
2023-09-06 20:30:35 -0700

    Add files via upload

---
+++ examenunidad1.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int num;
+int main()
+{
+    cout<<"Ingrese un número:";
+    cin >> num;
+    cout<< "El doble de un número es: " << num * 2;
+
+    return 0;
+}
